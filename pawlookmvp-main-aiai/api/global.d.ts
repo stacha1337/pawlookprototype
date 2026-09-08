e4c6cbf14652bd7c94c230b61c81d170649ae306
stacha1337 <rodzinaciesielscy2115@gmail.com>
2026-09-08 12:04:54 +0200

    Delete pawlookmvp-main-aiai directory

--- pawlookmvp-main-aiai/api/global.d.ts
+++
@@ -1,5 +0,0 @@
-// Minimal ambient declaration so we don't need @types/node as a dependency
-// just to read environment variables in the serverless function.
-declare const process: {
-  env: { [key: string]: string | undefined };
-};
